dab19e0c80f79c92d3c10d5379b3282bea928501
michaellin442 <michaellin442442@gmail.com>
2024-06-18 17:50:10 -0400

    added queries

--- mysql.updated.sql
+++ mysql.updated.sql
@@ -6,7 +6,17 @@ CREATE TABLE names (
 
 CREATE TABLE studies (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    study_name VARCHAR(255)
+    study_name VARCHAR(255),
+    study_category_id INT,
+    study_category VARCHAR(255),
+    study_status VARCHAR(255),
+    study_type VARCHAR(255),
+    start_year INT,
+    end_year INT,
+    sponsor_names VARCHAR(255),
+    no_of_countries INT,
+    no_of_sites INT,
+    no_of_participants INT
 );
 
 CREATE TABLE name_study (
@@ -16,7 +26,6 @@ CREATE TABLE name_study (
     FOREIGN KEY (name_id) REFERENCES names(id),
     FOREIGN KEY (study_id) REFERENCES studies(id)
 );
-
 
 CREATE TABLE funds (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -29,15 +38,64 @@ CREATE TABLE funds (
     month INT, 
     quarter VARCHAR(2),
     scenario VARCHAR(255),
-    FOREIGN KEY (study_id) REFERENCES name_study(id)
+    FOREIGN KEY (study_id) REFERENCES studies(id)
 );
 
-SELECT PI_name FROM names WHERE organization = "x";
-SELECT study_name FROM studies WHERE study_name = "x";
+/*queries*/
 
-SELECT ANY FROM studies WHERE revenue_or_expense = "x";
-SELECT ANY FROM studies WHERE fund_category = "x";
+/*display the organizations*/
+/*when clicking an organzation*/
+cursor.execute(   /*to get names of PIs that are part of the organization*/
+    SELECT PI_name
+    FROM names
+    WHERE organization = "selected_organzation"
+    )
+data = cursor.fetchall()
 
-SELECT funds.revenue_or_expense, funds.fund_category, funds.revenue_expense, funds.amount, funds.year, funds.month, funds.quarter
-FROM name_study
-INNER JOIN funds ON name_study.id = funds.id;
+/*display corresponding PI_names*/
+/*when clicking a PI_names*/
+
+cursor.execute(   /*to get study names that the PIs have worked on*/
+
+    SELECT study_name
+    FROM studies
+    WHERE id IN (
+        SELECT study_id
+        FROM name_study
+        WHERE name_id IN (
+            SELECT id
+            FROM names
+            WHERE PI_name = "selected_PI_name"
+        )
+    )
+)
+data = cursor.fetchall()
+
+/*display the study_names*/
+
+/*after clicking a study, display the info below,
+and an option to click "display funding info"*/
+
+cursor.execute(   /*to get study info */
+    SELECT * 
+    FROM studies
+    WHERE study_name = "selected_study_name"
+)
+
+
+/*if clicked "display funding info"*/
+cursor.execute(   /*to get study funding info */
+    SELECT * 
+    FROM funds
+    WHERE study_id IN (
+        SELECT id 
+        FROM studies
+        WHERE study_name =  "selected_study_name"
+    )
+)
+
+/*adding options to sort by revenue or expense, and by revenue/expense category.*/
+
+/*SELECT * FROM studies WHERE revenue_or_expense = "x"
+SELECT * FROM studies WHERE fund_category = "x"*/
+
